269e4df05a433a0b322c51e67f1509ab85d877cb
Pedro Mariano <>
2014-01-10 14:38:44 +0000

    First version of light and vibration modules.

---
+++ enki/interactions/LightSourceFromAbove.h
@@ -0,0 +1,53 @@
+/* 
+ * File:   LightSourceFromAbove.h
+ * Author: pedro
+ *
+ * Created on 8 de Janeiro de 2014, 15:54
+ */
+
+#ifndef LIGHTSOURCEFROMABOVE_H
+#define	LIGHTSOURCEFROMABOVE_H
+
+#include "LightSource.h"
+
+namespace Enki {
+
+	class LightSourceFromAbove:
+		public LightSource
+	{
+		/**
+		 * Shaping coefficient of the sigmoid function.
+		 */
+		const double k;
+		/**
+		 * Radius of the illuminated area.
+		 */
+		const double radius;
+		/**
+		 * Wave length with maximum intensity.
+		 */
+		const double peakWavelength;
+		/**
+		 * Maximum intensity of this light source.
+		 */
+		const double maxIntensity;
+		/**
+		 * Shaping coefficient of the Gaussian model.
+		 */
+		const double sigma;
+	public:
+		LightSourceFromAbove (double range, Robot* owner, Vector relativePosition,
+			double k, double radius, double peakWavelength, double maxIntensity,
+			double sigma);
+		LightSourceFromAbove (const LightSourceFromAbove& orig);
+		virtual ~LightSourceFromAbove ();
+
+		virtual double getIntensityAt (const Point& position, double wavelength) const;
+	private:
+
+	};
+	
+}
+
+#endif	/* LIGHTSOURCEFROMABOVE_H */
+
